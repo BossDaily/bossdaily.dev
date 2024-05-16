3a33a204132f76eeaa4c869124c49ce4a4e2d80b
BossDaily <gboys2008@gmail.com>
2024-05-16 16:48:35 -0500

    typewriter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Spotlight <br /> is the new trend.
+          BossDaily <br /> Web Developer.
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
           Spotlight effect is a great way to draw attention to a specific part
