d0101e74d8cde3050dc0b639061c2f1e944bc59c
BossDaily <gboys2008@gmail.com>
2024-08-23 17:10:59 -0500

    basic old config

---
+++ config.ts
@@ -0,0 +1,185 @@
+export const config = {
+  discordId: "274973338676494347",
+  projects: [
+    {
+      name: "Observer Bot",
+      url: "https://github.com/ComparatorCraftSMP/observer-bot-ts",
+      featured: true,
+      btnName: "Repo",
+      thumbnail:
+        "https://cdn.discordapp.com/attachments/765808912355819564/1172286029563183165/Screenshot_2023-11-09_152212.png?ex=655fc371&is=654d4e71&hm=bc97a42d29f924be353d0c3a975d28b8e43309855b7c79e08b9933fcf6905e9f&",
+      description: `Observer Bot is a Discord bot I built for my Minecraft server in
+      Discord.js. This bot mainly queries stats from a Minecraft server
+      with ServerTAP installed and shows them in a Discord embed. I made
+      this because a lot of other solutions to do this sucked so I
+      thought I would make one myself. The bot also shows information
+      about the Discord server it is on, and members in the Discord
+      server. And soon it will be able to show stats about the Minecraft
+      server.`,
+      tags: [
+        "Discord.js",
+        "Typescript",
+        "Node.js",
+        "Minecraft",
+        "REST-API",
+        "SQLite",
+      ],
+    },
+    {
+      name: "Analog",
+      url: "https://github.com/analog-org",
+      featured: true,
+      btnName: "Repo",
+      thumbnail:
+        "https://cdn.discordapp.com/attachments/765802538023780353/1172281499970711643/Analog_v2_Dark_theme_Desktop_screensot.png?ex=655fbf39&is=654d4a39&hm=40ce1d70ea26875f70c6b495cac3866fd4e90875ca2103c9be6a2511da8b2fb6&",
+      description: `Analog is a project im working on that will be the best open
+      source discord bot available. I intend it to be easy to self-host,
+      have a webpanel, and have a plugin system/plugin market place. Im
+      working on building a bot template for building Discord bots in
+      Typescript, and another one for building bots with Typescript and
+      NextJS. Analog will also need a plugin repo, a landing page, and
+      several documentation pages.`,
+      tags: [
+        "Discord.js",
+        "Typescript",
+        "Sapphire.js",
+        "REST-APIs",
+        "Prisma",
+        "MySQL",
+        "Svelte",
+        "Svelte-Kit",
+        "TailwindCSS",
+      ],
+    },
+    {
+      name: "S.I.M.P.S",
+      url: "https://github.com/simpsmc",
+      featured: true,
+      btnName: "Repo",
+      thumbnail:
+        "https://cdn.discordapp.com/attachments/765808912355819564/1172288286597853315/Screenshot_2023-11-09_153052.png?ex=655fc58b&is=654d508b&hm=c7d028b0f6d974a16651b63449c76eefc52b2c1de5f9fbd0508510f34b35711a&",
+      description: `S.I.M.P.S. stands for Server Integrated Multi Punishment System,
+      aims to be an innovative and new global ban list of problematic
+      players in smaller Minecraft SMP communities. I built them a
+      Discord bot that admins can use to add, edit, and view the database of
+      players from Discord. This bot is functional but the repo is private at the moment.`,
+      tags: [
+        "Discord.js",
+        "Typescript",
+        "REST-APIs",
+        "Prisma",
+        "MySQL",
+        "Minecraft",
+      ],
+    },
+    {
+      name: "Discord Link Embed",
+      url: "https://discord-link-kappa.vercel.app/",
+      featured: true,
+      btnName: "Website",
+      thumbnail:
+        "https://cdn.discordapp.com/attachments/765801416680931331/1172290865004298360/Screenshot_2023-11-09_154318.png?ex=655fc7f2&is=654d52f2&hm=62838e45128afab31249e4418fdef4ef21fe46ed171eb215ada9082b3ca0979f&",
+      description: `Discord link embed is a website that allows you to dynamically create an embed on Discord only using a URL. 
+      It uses NextJS's dynamic routes to create a page for each URL, with opengraph and twitter metadata.`,
+      tags: ["Next.JS", "Typescript", "REST-APIs", "TailwindCSS"],
+    },
+  ],
+  languages: [
+    {
+      type: "Front-End",
+      languages: [
+        {
+          name: "React",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        },
+        {
+          name: "NextJS",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+        },
+        {
+          name: "TailwindCSS",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        },
+        {
+          name: "HTML",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+          name: "CSS",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        },
+        {
+          name: "Javascript",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        },
+        {
+          name: "Typescript",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        },
+      ],
+    },
+    {
+      type: "Backend",
+      languages: [
+        {
+          name: "Node.JS",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        },
+        {
+          name: "MySQL",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        },
+        {
+          name: "Prisma",
+          img: "https://seeklogo.com/images/P/prisma-logo-3805665B69-seeklogo.com.png",
+        },
+        {
+          name: "Discord.JS",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/discordjs/discordjs-original.svg",
+        },
+      ],
+    },
+    {
+      type: "Tools",
+      languages: [
+        {
+          name: "Git",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+        },
+        {
+          name: "VSCode",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+        },
+        {
+          name: "Linux",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+        },
+        {
+          name: "Figma",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+        },
+        {
+          name: "Github",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+        {
+          name: "IntelliJ",
+          img: "https://resources.jetbrains.com/storage/products/company/brand/logos/IntelliJ_IDEA_icon.svg",
+        },
+      ],
+    },
+    {
+      type: "Productivity",
+      languages: [
+        {
+          name: "Trello",
+          img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg",
+        },
+        {
+          name: "Notion",
+          img: "https://media.discordapp.net/attachments/965985896967077888/1024337530247729162/notion-logo-no-background.png",
+        },
+      ],
+    },
+  ],
+}
