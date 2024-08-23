a6771f453eda93fe7245915f03cebfed543b44f9
BossDaily <gboys2008@gmail.com>
2024-08-23 16:55:07 -0500

    lanyard integration

--- src/components/hexta-ui/Navbar.tsx
+++ src/components/hexta-ui/Navbar.tsx
@@ -7,9 +7,16 @@ import Image from "next/legacy/image";
 import Link from "next/link";
 
 import { FaHome, FaPhone, FaRocket, FaUser } from "react-icons/fa";
+import { Avatar } from "./Avatar";
+
+import { useLanyard } from "react-use-lanyard";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { loading, status } = useLanyard({
+		userId: "274973338676494347",
+		socket: true,
+	});
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -17,9 +24,9 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="flex justify-between px-5 py-4 top-4 z-50 sticky mx-auto max-w-5xl items-center overflow-hidden rounded-[20px] bg-Windsor/[0.1] border border-white/[0.01] backdrop-blur-[6px] text-white">
+      <nav className="flex justify-between px-5 py-3 top-4 z-50 sticky mx-auto max-w-5xl items-center overflow-hidden rounded-[20px] bg-Windsor/[0.1] border border-white/[0.01] backdrop-blur-[6px] text-white">
         <div>
-          <Image src="/hexta-studio.svg" alt="HextaUI" width={35} height={35} />
+          <Avatar avatarUrl={`https://cdn.discordapp.com/avatars/${status?.discord_user.id}/${status?.discord_user.avatar}.png?size=1024&format=webp&quality=lossless&width=0&height=256`} size={48} variant="withStatus"/>
         </div>
         <div className="flex items-center">
           <div className="relative">
