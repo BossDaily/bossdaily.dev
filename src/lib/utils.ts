a8fa6b585f1b57c3c3d95a10bd8a9c12a2927587
BossDaily <gboys2008@gmail.com>
2024-08-24 17:07:40 -0500

    uses proper styling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,22 +6,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function statusColors(status: LanyardData) {
-  if (status.discord_status === "online") return "bg-green-500";
-  else if (status.discord_status === "dnd") return "bg-red-500";
-  else if (status.discord_status === "idle") return "bg-yellow-500";
-  else return "bg-gray-500";
+export function statusColors(status: LanyardData | undefined) {
+  if (status?.discord_status === "online") return "green-500";
+  else if (status?.discord_status === "dnd") return "red-500";
+  else if (status?.discord_status === "idle") return "yellow-500";
+  else return "gray-500";
 }
 
-export function statusText(status: LanyardData) {
-  if (status.active_on_discord_desktop || status.active_on_discord_web) {
-    if (status.discord_status === "online") return "Online on Desktop";
-    else if (status.discord_status === "dnd")
+export function statusText(status: LanyardData | undefined) {
+  if (status?.active_on_discord_desktop || status?.active_on_discord_web) {
+    if (status?.discord_status === "online") return "Online on Desktop";
+    else if (status?.discord_status === "dnd")
       return "Do Not Disturb on Desktop";
-    else if (status.discord_status === "idle") return "Idle on Desktop";
-  } else if (status.active_on_discord_mobile) {
-    if (status.discord_status === "online") return "Online on Mobile";
-    else if (status.discord_status === "dnd") return "Do Not Disturb on Mobile";
-    else if (status.discord_status === "idle") return "Idle on Mobile";
+    else if (status?.discord_status === "idle") return "Idle on Desktop";
+  } else if (status?.active_on_discord_mobile) {
+    if (status?.discord_status === "online") return "Online on Mobile";
+    else if (status?.discord_status === "dnd") return "Do Not Disturb on Mobile";
+    else if (status?.discord_status === "idle") return "Idle on Mobile";
   } else return "Offline";
 }
