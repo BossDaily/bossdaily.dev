cbe45465bda1ef2ac280a0470cbc0a3eb2626b52
BossDaily <gboys2008@gmail.com>
2024-08-24 15:45:54 -0500

    status colors function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { DiscordUser, LanyardData } from "react-use-lanyard";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function statusColors(status: LanyardData) {
+  if (status.discord_status === "online") return "bg-green-500";
+  else if (status.discord_status === "dnd") return "bg-red-500";
+  else if (status.discord_status === "idle") return "bg-yellow-500";
+  else return "bg-gray-500";
 }
