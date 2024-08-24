a9823091aa0f1285c45c1bbb48223ea7a03b03ea
BossDaily <gboys2008@gmail.com>
2024-08-24 15:54:23 -0500

    status text function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,16 @@ export function statusColors(status: LanyardData) {
   else if (status.discord_status === "idle") return "bg-yellow-500";
   else return "bg-gray-500";
 }
+
+export function statusText(status: LanyardData) {
+  if (status.active_on_discord_desktop || status.active_on_discord_web) {
+    if (status.discord_status === "online") return "Online on Desktop";
+    else if (status.discord_status === "dnd")
+      return "Do Not Disturb on Desktop";
+    else if (status.discord_status === "idle") return "Idle on Desktop";
+  } else if (status.active_on_discord_mobile) {
+    if (status.discord_status === "online") return "Online on Mobile";
+    else if (status.discord_status === "dnd") return "Do Not Disturb on Mobile";
+    else if (status.discord_status === "idle") return "Idle on Mobile";
+  } else return "Offline";
+}
