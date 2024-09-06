1310cb4e595c3ff29d91be179bc8d3421a6083f5
BossDaily <gboys2008@gmail.com>
2024-09-06 17:15:01 -0500

    Fixed styling and made card mobile responsive

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -16,8 +16,10 @@ import { statusColors, statusText } from "@/lib/utils";
 import { cn } from "@/lib/utils";
 import { HeaderText } from "./HeaderText";
 import HyperText from "./magicui/hyper-text";
-import { Card, CardContent, CardFooter } from "./ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import Image from "next/image";
+import { SiGithub } from '@icons-pack/react-simple-icons';
+import { Badge } from "./ui/badge";
 
 export const Projects: React.FC = () => {
   const { loading, status } = useLanyard({
@@ -27,9 +29,9 @@ export const Projects: React.FC = () => {
   return (
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
       <HeaderText>Projects</HeaderText>
-      <div className="flex justify-between gap-10">
-        <Card className="h-full w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian">
-          <div className="p-4">
+      <div className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10">
+        <Card className="h-full w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group">
+          <div className="p-4 overflow-hidden">
             <Image
               src="/project-analog.png"
               alt="project image"
@@ -38,19 +40,51 @@ export const Projects: React.FC = () => {
               className="rounded-xl"
             />
           </div>
+          <CardHeader className="py-0 text-white">
+            <a className="flex flex-row gap-2 hover:underline" href="https://github.com/BossDaily/bossdaily.dev"><CardTitle>Analog Svelte</CardTitle> <SiGithub /> </a>
+            <CardDescription className="text-zinc-400">
+              A project that is analog in nature.
+            </CardDescription>
+            <CardFooter className="p-0 pt-2 gap-2 overflow-hidden">
+              <Badge className="bg-Purple-Heart">React</Badge>
+              <Badge className="bg-Purple-Heart">TypeScript</Badge>
+              <Badge className="bg-Purple-Heart">SvelteKit</Badge>
+              <Badge className="bg-Purple-Heart">Tailwind CSS</Badge>
 
-          <CardContent className="py-5"></CardContent>
-          <CardFooter>Hi</CardFooter>
+
+            </CardFooter>
+          </CardHeader>
+
+          <CardContent className="p-0"></CardContent>
+          <CardFooter></CardFooter>
         </Card>
-        <Card className="h-full w-4/6 overflow-hidden">
-          <CardContent>
+        <Card className="h-full w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group">
+          <div className="p-4 overflow-hidden">
             <Image
-              src="/project-analog.png"
+              src="/project-embed.png"
               alt="project image"
               height={1280}
               width={832}
+              className="rounded-xl"
             />
-          </CardContent>
+          </div>
+          <CardHeader className="py-0 text-white">
+            <a className="flex flex-row gap-2 hover:underline" href="https://github.com/BossDaily/bossdaily.dev"><CardTitle>Embed </CardTitle> <SiGithub /> </a>
+            <CardDescription className="text-zinc-400">
+              Generate a Discord Embed with a simple url
+            </CardDescription>
+            <CardFooter className="p-0 pt-2 gap-2 overflow-hidden">
+              <Badge className="bg-Purple-Heart">React</Badge>
+              <Badge className="bg-Purple-Heart">TypeScript</Badge>
+              <Badge className="bg-Purple-Heart">Next.js</Badge>
+              <Badge className="bg-Purple-Heart">Tailwind CSS</Badge>
+
+
+            </CardFooter>
+          </CardHeader>
+
+          <CardContent className="p-0"></CardContent>
+          <CardFooter></CardFooter>
         </Card>
       </div>
     </div>
