2e463fd677bff6b0e24619fd289d298184683f3e
BossDaily <gboys2008@gmail.com>
2024-09-06 17:53:02 -0500

    Removed useless imports

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,21 +1,8 @@
 "use client";
 
-import { Spotlight } from "./ui/Spotlight";
-import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
 import { config } from "../../config";
-import { FlipWords } from "./ui/flip-words";
-import { Avatar } from "./hexta-ui/Avatar";
 import { useLanyard } from "react-use-lanyard";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { statusColors, statusText } from "@/lib/utils";
-import { cn } from "@/lib/utils";
 import { HeaderText } from "./HeaderText";
-import HyperText from "./magicui/hyper-text";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import Image from "next/image";
 import { SiGithub } from '@icons-pack/react-simple-icons';
@@ -28,7 +15,7 @@ export const Projects: React.FC = () => {
   });
   return (
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
-      <HeaderText>Projects</HeaderText>
+      <HeaderText>Featured Projects</HeaderText>
       <div className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10">
         <Card className="h-full w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group">
           <div className="p-4 overflow-hidden">
