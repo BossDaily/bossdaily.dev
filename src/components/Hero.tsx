647ff1efb6805caccbaac7eed630e92318c71146
BossDaily <gboys2008@gmail.com>
2024-09-06 23:17:48 -0500

    Added framer motion animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,19 +14,50 @@ import {
 } from "@/components/ui/tooltip";
 import { statusColors, statusText } from "@/lib/utils";
 import { cn } from "@/lib/utils";
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 export const Hero: React.FC = () => {
   const { loading, status } = useLanyard({
     userId: config.discordId,
     socket: true,
   });
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    void animate(
+      [
+        [scope.current, { y: "0%" }, { duration: 0 }],
+        [scope.current, { y: "-25%" }, { duration: 0.3, at: "+1.3" }],
+        [scope.current, { y: "-50%" }, { duration: 0.3, at: "+1.3" }],
+        [scope.current, { y: "-75%" }, { duration: 0.3, at: "+1.3" }],
+      ],
+      {
+        repeat: Number.POSITIVE_INFINITY,
+      }
+    );
+  }, [animate, scope]);
+
   return (
     <div className="z-20  h-full max-w-full items-center sm:max-w-5xl gap-8 my-8 sm:my-16 flex flex-col sm:flex-row sm:items-center sm:justify-between relative overflow-visible p-4 align-middle mx-auto py-12 sm:py-24">
       <Spotlight
         className="-top-20 sm:-top-40 left-0 sm:left-60 sm:-top-20"
         fill="#C77DFF"
       />
-      <div className="mx-auto relative w-full pt-10 sm:pt-20 md:pt-0 gap-1 flex flex-col items-center sm:items-start">
+      <motion.div
+        className="mx-auto relative w-full pt-10 sm:pt-20 md:pt-0 gap-1 flex flex-col items-center sm:items-start"
+        initial={{
+          y: 40,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-center sm:text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Hello, I am BossDaily <br />{" "}
           <FlipWords
@@ -34,24 +65,42 @@ export const Hero: React.FC = () => {
             className="text-white text-2xl sm:text-3xl"
           />
         </h1>
-      </div>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <Avatar
-              avatarUrl="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=1024&format=webp&quality=lossless&width=0&height=256"
-              size={120}
-              className={cn(
-                "hover:ring-4",
-                `hover:ring-${statusColors(status)}`
-              )}
-            />
-          </TooltipTrigger>
-          <TooltipContent side="bottom" sideOffset={16} className="border-none text-white bg-Windsor">
-            <p>{statusText(status)}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      </motion.div>
+      <motion.div
+        initial={{
+          y: 40,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Avatar
+                avatarUrl="https://cdn.discordapp.com/avatars/274973338676494347/00dcf84af54a0a58d2394b4054e0f7f5.png?size=1024&format=webp&quality=lossless&width=0&height=256"
+                size={120}
+                className={cn(
+                  "hover:ring-4",
+                  `hover:ring-${statusColors(status)}`
+                )}
+              />
+            </TooltipTrigger>
+            <TooltipContent
+              side="bottom"
+              sideOffset={16}
+              className="border-none text-white bg-Windsor"
+            >
+              <p>{statusText(status)}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </motion.div>
     </div>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,16 +15,47 @@ import Image from "next/image";
 import { SiGithub } from "@icons-pack/react-simple-icons";
 import { Badge } from "./ui/badge";
 import ShinyButton from "./magicui/shiny-button";
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 export const Projects: React.FC = () => {
   const { loading, status } = useLanyard({
     userId: config.discordId,
     socket: true,
   });
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    void animate(
+      [
+        [scope.current, { y: "0%" }, { duration: 0 }],
+        [scope.current, { y: "-25%" }, { duration: 0.3, at: "+1.3" }],
+        [scope.current, { y: "-50%" }, { duration: 0.3, at: "+1.3" }],
+        [scope.current, { y: "-75%" }, { duration: 0.3, at: "+1.3" }],
+      ],
+      {
+        repeat: Number.POSITIVE_INFINITY,
+      }
+    );
+  }, [animate, scope]);
+
   return (
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
       <HeaderText>Featured Projects</HeaderText>
-      <div className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10">
+      <motion.div
+        className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10"
+        initial={{
+          y: 40,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+      >
         {config.projects.map((project, index) => {
           if (project.featured === true) {
             return (
@@ -68,7 +99,7 @@ export const Projects: React.FC = () => {
             );
           }
         })}
-      </div>
+      </motion.div>
       <a title="View Projects" href="https://github.com/bossdaily">
         <ShinyButton text="View All Projects" className="dark" />
       </a>
