b0ee5a1317da4742f2846532da9dc417ef9330c6
BossDaily <gboys2008@gmail.com>
2024-09-06 22:57:38 -0500

    fixed config

--- config.ts
+++ config.ts
@@ -10,10 +10,10 @@ export const config = {
     {
       name: "Observer Bot",
       url: "https://github.com/ComparatorCraftSMP/observer-bot-ts",
-      featured: true,
+      featured: false,
       btnName: "Repo",
       thumbnail:
-        "https://cdn.discordapp.com/attachments/765808912355819564/1172286029563183165/Screenshot_2023-11-09_152212.png?ex=655fc371&is=654d4e71&hm=bc97a42d29f924be353d0c3a975d28b8e43309855b7c79e08b9933fcf6905e9f&",
+        "/project-observer-bot.png",
       description: `Observer Bot is a Discord bot I built for my Minecraft server in
       Discord.js. This bot mainly queries stats from a Minecraft server
       with ServerTAP installed and shows them in a Discord embed. I made
@@ -37,33 +37,24 @@ export const config = {
       featured: true,
       btnName: "Repo",
       thumbnail:
-        "https://cdn.discordapp.com/attachments/765802538023780353/1172281499970711643/Analog_v2_Dark_theme_Desktop_screensot.png?ex=655fbf39&is=654d4a39&hm=40ce1d70ea26875f70c6b495cac3866fd4e90875ca2103c9be6a2511da8b2fb6&",
-      description: `Analog is a project im working on that will be the best open
-      source discord bot available. I intend it to be easy to self-host,
-      have a webpanel, and have a plugin system/plugin market place. Im
-      working on building a bot template for building Discord bots in
-      Typescript, and another one for building bots with Typescript and
-      NextJS. Analog will also need a plugin repo, a landing page, and
-      several documentation pages.`,
+        "/project-analog.png",
+      description: `Analog will be a fullstack, self hosted Discord bot with a web dashboard built in Svelte-Kit.`,
       tags: [
         "Discord.js",
         "Typescript",
-        "Sapphire.js",
-        "REST-APIs",
         "Prisma",
         "MySQL",
-        "Svelte",
-        "Svelte-Kit",
+        "SvelteKit",
         "TailwindCSS",
       ],
     },
     {
       name: "S.I.M.P.S",
       url: "https://github.com/simpsmc",
-      featured: true,
+      featured: false,
       btnName: "Repo",
       thumbnail:
-        "https://cdn.discordapp.com/attachments/765808912355819564/1172288286597853315/Screenshot_2023-11-09_153052.png?ex=655fc58b&is=654d508b&hm=c7d028b0f6d974a16651b63449c76eefc52b2c1de5f9fbd0508510f34b35711a&",
+        "/project-simps.png",
       description: `S.I.M.P.S. stands for Server Integrated Multi Punishment System,
       aims to be an innovative and new global ban list of problematic
       players in smaller Minecraft SMP communities. I built them a
@@ -84,10 +75,9 @@ export const config = {
       featured: true,
       btnName: "Website",
       thumbnail:
-        "https://cdn.discordapp.com/attachments/765801416680931331/1172290865004298360/Screenshot_2023-11-09_154318.png?ex=655fc7f2&is=654d52f2&hm=62838e45128afab31249e4418fdef4ef21fe46ed171eb215ada9082b3ca0979f&",
-      description: `Discord link embed is a website that allows you to dynamically create an embed on Discord only using a URL. 
-      It uses NextJS's dynamic routes to create a page for each URL, with opengraph and twitter metadata.`,
-      tags: ["Next.JS", "Typescript", "REST-APIs", "TailwindCSS"],
+        "/project-embed.png",
+      description: `This website allows you to dynamically create an embed on Discord only using a URL.`,
+      tags: ["Next.JS", "Typescript", "TailwindCSS"],
     },
   ],
   languages: [
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,9 +3,16 @@
 import { config } from "../../config";
 import { useLanyard } from "react-use-lanyard";
 import { HeaderText } from "./HeaderText";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import Image from "next/image";
-import { SiGithub } from '@icons-pack/react-simple-icons';
+import { SiGithub } from "@icons-pack/react-simple-icons";
 import { Badge } from "./ui/badge";
 
 export const Projects: React.FC = () => {
@@ -17,62 +24,50 @@ export const Projects: React.FC = () => {
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
       <HeaderText>Featured Projects</HeaderText>
       <div className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10">
-        <Card className="h-full w-10/12 md:w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group">
-          <div className="p-4 overflow-hidden">
-            <Image
-              src="/project-analog.png"
-              alt="project image"
-              height={1280}
-              width={832}
-              className="rounded-xl"
-            />
-          </div>
-          <CardHeader className="py-0 text-white">
-            <a className="flex flex-row gap-2 hover:underline" href="https://github.com/BossDaily/bossdaily.dev"><CardTitle>Analog Svelte</CardTitle> <SiGithub /> </a>
-            <CardDescription className="text-zinc-400">
-              A project that is analog in nature.
-            </CardDescription>
-            <CardFooter className="p-0 pt-2 gap-2 overflow-hidden">
-              <Badge className="bg-Purple-Heart">React</Badge>
-              <Badge className="bg-Purple-Heart">TypeScript</Badge>
-              <Badge className="bg-Purple-Heart">SvelteKit</Badge>
-              <Badge className="bg-Purple-Heart">Tailwind CSS</Badge>
+        {config.projects.map((project, index) => {
+          if (project.featured === true) {
+            return (
+              <Card
+                key={index}
+                className="h-max w-7/12 md:w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group"
+              >
+                <div className="p-4 overflow-hidden">
+                  <Image
+                    src={project.thumbnail}
+                    alt="project image"
+                    height={1280}
+                    width={832}
+                    className="rounded-xl"
+                  />
+                </div>
+                <CardHeader className="py-0 text-white">
+                  <a
+                    className="flex flex-row gap-2 hover:underline"
+                    href={project.url}
+                  >
+                    <CardTitle>{project.name}</CardTitle> <SiGithub />{" "}
+                  </a>
+                  <CardDescription className="text-zinc-400">
+                    {project.description}
+                  </CardDescription>
+                  <CardFooter className="p-0 pt-2 gap-2 overflow-x-scroll">
+                    {project.tags.map((tag, index) => {
+                      return (
+                        <Badge key={index} className="bg-Purple-Heart">
+                          {tag}
+                        </Badge>
+                      );
+                    })}
+                  </CardFooter>
+                </CardHeader>
 
-
-            </CardFooter>
-          </CardHeader>
-
-          <CardContent className="p-0"></CardContent>
-          <CardFooter></CardFooter>
-        </Card>
-        <Card className="h-full w-10/12 md:w-4/6 overflow-hidden shadow-feature-card-dark bg-BlackRussian group ">
-          <div className="p-4 overflow-hidden">
-            <Image
-              src="/project-embed.png"
-              alt="project image"
-              height={1280}
-              width={832}
-              className="rounded-xl"
-            />
-          </div>
-          <CardHeader className="py-0 text-white">
-            <a className="flex flex-row gap-2 hover:underline" href="https://github.com/BossDaily/bossdaily.dev"><CardTitle>Embed </CardTitle> <SiGithub /> </a>
-            <CardDescription className="text-zinc-400">
-              Generate a Discord Embed with a simple url
-            </CardDescription>
-            <CardFooter className="p-0 pt-2 gap-2 overflow-hidden">
-              <Badge className="bg-Purple-Heart">React</Badge>
-              <Badge className="bg-Purple-Heart">TypeScript</Badge>
-              <Badge className="bg-Purple-Heart">Next.js</Badge>
-              <Badge className="bg-Purple-Heart">Tailwind CSS</Badge>
-
-
-            </CardFooter>
-          </CardHeader>
-
-          <CardContent className="p-0"></CardContent>
-          <CardFooter></CardFooter>
-        </Card>
+                <CardContent className="p-0"></CardContent>
+                <CardFooter></CardFooter>
+              </Card>
+            );
+          }
+        })}
+      
       </div>
     </div>
   );
