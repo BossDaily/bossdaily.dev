6896f9549951241ee860735ff24393750fdec08e
BossDaily <gboys2008@gmail.com>
2024-10-12 17:40:28 -0500

    fixed type error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,16 +15,18 @@ import {
 import { statusColors, statusText } from "@/lib/utils";
 import { cn } from "@/lib/utils";
 import { motion, useAnimate } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const Hero: React.FC = () => {
   const { loading, status } = useLanyard({
     userId: config.discordId,
     socket: true,
   });
-  const [scope, animate] = useAnimate();
+/*   const [scope, animate] = useAnimate();
 
+  // @ts-ignore
   useEffect(() => {
+    // @ts-ignore
     void animate(
       [
         [scope.current, { y: "0%" }, { duration: 0 }],
@@ -36,7 +38,7 @@ export const Hero: React.FC = () => {
         repeat: Number.POSITIVE_INFINITY,
       }
     );
-  }, [animate, scope]);
+  }, [animate, scope]); */
 
   return (
     <div className="z-20  h-full max-w-full items-center sm:max-w-5xl gap-8 my-8 sm:my-16 flex flex-col sm:flex-row sm:items-center sm:justify-between relative overflow-visible p-4 align-middle mx-auto py-12 sm:py-24">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,7 @@ export const Projects: React.FC = () => {
     userId: config.discordId,
     socket: true,
   });
-  const [scope, animate] = useAnimate();
+ /*  const [scope, animate] = useAnimate();
 
   useEffect(() => {
     void animate(
@@ -37,7 +37,7 @@ export const Projects: React.FC = () => {
         repeat: Number.POSITIVE_INFINITY,
       }
     );
-  }, [animate, scope]);
+  }, [animate, scope]); */
 
   return (
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
