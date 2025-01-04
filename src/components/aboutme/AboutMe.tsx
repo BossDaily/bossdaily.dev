30b44c0ec866c5d75e4fd05342a656115f6179c3
BossDaily <gboys2008@gmail.com>
2025-01-04 13:36:07 -0600

    shows local time and added placeholder github cards

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -21,18 +21,21 @@ import LanCard from "./LanCard";
 import Globe from "./globe";
 import { WakatimeData } from "./wakatimeInterface";
 import NumberTicker from "../ui/number-ticker";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 interface AboutMeProps {
   wakatimeData: WakatimeData | null;
 }
 export const AboutMe: React.FC<AboutMeProps> = ({ wakatimeData }) => {
-
-  
   /* const { loading, status } = useLanyard({
     userId: config.discordId,
     socket: true,
   }); */
-
 
   /*  const [scope, animate] = useAnimate();
 
@@ -49,31 +52,64 @@ export const AboutMe: React.FC<AboutMeProps> = ({ wakatimeData }) => {
       }
     );
   }, [animate, scope]); */
-  const totalHrs = wakatimeData?.data.total_seconds ? Math.round(wakatimeData.data.total_seconds / 3600): 0
+  const totalHrs = wakatimeData?.data.total_seconds
+    ? Math.round(wakatimeData.data.total_seconds / 3600)
+    : 0;
 
-  const totalProjects = wakatimeData?.data.projects ? wakatimeData.data.projects.length : 0
+  const totalProjects = wakatimeData?.data.projects
+    ? wakatimeData.data.projects.length
+    : 0;
 
   return (
     <div className="z-20 h-full max-w-full items-center sm:max-w-5xl flex flex-col sm:items-center sm:justify-center relative  align-middle mx-auto py-12 sm:py-24">
-      <HeaderText>Featured Projects</HeaderText>
+      <HeaderText>About Me</HeaderText>
       <div className="grid h-full w-full gap-4 p-2 grid-cols-12 grid-rows-2 rounded-lg text-white">
         <div className="col-span-3 row-span-1 shadow-feature-card-dark bg-BlackRussian group rounded-lg flex items-center justify-center overflow-hidden">
-            <p className="text-2xl">
-            <NumberTicker className="text-white tracking-tighter" value={totalHrs} /> hours
-            </p>
+          <p className="text-2xl">
+            <NumberTicker
+              className="text-white tracking-tighter"
+              value={totalHrs}
+            />{" "}
+            hours
+          </p>
         </div>
 
         <div className="col-span-6 row-span-1 shadow-feature-card-dark bg-BlackRussian group rounded-lg flex items-center justify-center overflow-hidden relative h-48">
-          <p className="absolute top-2 right-4 z-10">Broccoli</p>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <p className="absolute top-2 right-4 z-10">Based in Texas</p>
+              </TooltipTrigger>
+              <TooltipContent
+                side="bottom"
+                sideOffset={16}
+                className="border-none text-white bg-Windsor"
+              >
+                <p>
+                  Time for me:{" "}
+                  {new Date().toLocaleTimeString("en-US", {
+                    timeZone: "America/Chicago",
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  })}{" "}
+                  (CST){" "}
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
           <div className="absolute inset-0">
             <Globe />
           </div>
         </div>
 
         <div className="col-span-3 row-span-4 shadow-feature-card-dark bg-BlackRussian group rounded-lg flex items-center justify-center overflow-hidden">
-            <div className="relative w-full">
+        
+          <div className="relative w-full flex flex-col items-end justify-center p-2 gap-1">
+          <p className="  right-4 z-10">Github Stats (Placeholder)</p>
             <Image
-              src="https://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=bossdaily&theme=tokyonight"
+              src="https://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=bossdaily&theme=aura"
               alt="Picture of the author"
               width={300}
               height={200}
@@ -81,19 +117,27 @@ export const AboutMe: React.FC<AboutMeProps> = ({ wakatimeData }) => {
               sizes="(min-width: 808px) 50vw, 100vw"
               className="object-cover"
             />
-            </div>
+            <Image
+              src="https://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=bossdaily&theme=aura"
+              alt="Picture of the author"
+              width={300}
+              height={200}
+              priority
+              sizes="(min-width: 808px) 50vw, 100vw"
+              className="object-cover"
+            />
+            <Image
+              src="https://github-profile-summary-cards.vercel.app/api/cards/stats?username=bossdaily&theme=aura"
+              alt="Picture of the author"
+              width={300}
+              height={200}
+              priority
+              sizes="(min-width: 808px) 50vw, 100vw"
+              className="object-cover"
+            />
+          </div>
 
-            <div className="relative w-full">
-            <Image
-              src="https://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=bossdaily&theme=tokyonight"
-              alt="Picture of the author"
-              width={300}
-              height={200}
-              priority
-              sizes="(min-width: 808px) 50vw, 100vw"
-              className="object-cover"
-            />
-            </div>
+
           {/* <LanCard /> */}
         </div>
 
@@ -102,7 +146,13 @@ export const AboutMe: React.FC<AboutMeProps> = ({ wakatimeData }) => {
         </div>
 
         <div className="col-span-3 row-span-2 shadow-feature-card-dark bg-BlackRussian group rounded-lg flex items-center justify-center overflow-hidden">
-          <p><NumberTicker className="text-white tracking-tighter  text-2xl" value={totalProjects} /> Projects</p>
+          <p>
+            <NumberTicker
+              className="text-white tracking-tighter  text-2xl"
+              value={totalProjects}
+            />{" "}
+            Projects
+          </p>
         </div>
       </div>
 
