8c13d2536955a73b897c62dc752f8e96fa465396
BossDaily <gboys2008@gmail.com>
2025-01-03 21:04:13 -0600

    reconfigured next image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{
+      hostname: 'github-profile-summary-cards.vercel.app',
+      protocol: 'https',
+    }],
+  },
+}
 
 export default nextConfig;
