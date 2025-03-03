e0001ce7411a749682ce1cff5bee83990e41dfd0
BossDaily <gboys2008@gmail.com>
2025-03-03 13:21:12 -0600

    Project card is mobile responsive

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,10 +18,10 @@ import { motion } from "framer-motion";
 
 export const Projects: React.FC = () => {
   return (
-    <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col  sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
+    <div className="z-20 h-full max-w-full items-center sm:max-w-5xl gap-8 flex flex-col sm:items-center sm:justify-center relative p-4 align-middle mx-auto py-12 sm:py-24">
       <HeaderText>Featured Projects</HeaderText>
       <motion.div
-        className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10"
+        className="flex md:flex-row flex-col justify-center md:justify-between align-middle items-center gap-10 w-full"
         initial={{
           y: 40,
           opacity: 0,
@@ -39,15 +39,15 @@ export const Projects: React.FC = () => {
             return (
               <Card
                 key={index}
-                className="h-max w-full md:w-[480px] overflow-hidden shadow-feature-card-dark bg-BlackRussian group"
+                className="h-max w-[320px] sm:w-[400px] md:w-[480px] overflow-hidden shadow-feature-card-dark bg-BlackRussian group"
               >
-                <div className="p-4 overflow-hidden">
+                <div className="p-2 sm:p-4 overflow-hidden">
                   <Image
                     src={project.thumbnail}
                     alt="project image"
                     height={1280}
                     width={832}
-                    className="rounded-xl"
+                    className="rounded-xl w-full h-auto object-cover"
                   />
                 </div>
                 <CardHeader className="py-0 text-white">
