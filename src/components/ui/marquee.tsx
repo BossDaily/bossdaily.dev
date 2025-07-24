cc7f6e8063b525778f93ec56e3b024bbf1634ccf
BossDaily <gboys2008@gmail.com>
2025-07-24 17:56:51 -0500

    fixed marque and spotlight animation

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -45,25 +45,31 @@ export default function Marquee({
     <div
       {...props}
       className={cn(
-        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] gap-(--gap)",
+        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem]",
         {
           "flex-row": !vertical,
           "flex-col": vertical,
         },
         className,
       )}
+      style={{
+        gap: "var(--gap)",
+      }}
     >
       {Array(repeat)
         .fill(0)
         .map((_, i) => (
           <div
             key={i}
-            className={cn("flex shrink-0 justify-around gap-(--gap)", {
+            className={cn("flex shrink-0 justify-around", {
               "animate-marquee flex-row": !vertical,
               "animate-marquee-vertical flex-col": vertical,
               "group-hover:[animation-play-state:paused]": pauseOnHover,
               "[animation-direction:reverse]": reverse,
             })}
+            style={{
+              gap: "var(--gap)",
+            }}
           >
             {children}
           </div>
