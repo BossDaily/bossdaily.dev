bfe83b52b14e28effc5f58fe0ab9badb1ba784c8
BossDaily <gboys2008@gmail.com>
2025-07-24 18:10:28 -0500

    added light mode compatibility

--- src/components/aboutme/LanguagesDialog.tsx
+++ src/components/aboutme/LanguagesDialog.tsx
@@ -33,17 +33,17 @@ export const LanguagesDialog: React.FC<LanguagesDialogProps> = ({ trigger }) => 
   return (
     <Dialog>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent className="max-w-[90vw] sm:max-w-3xl bg-BlackRussian overflow-y-auto max-h-[90vh]">
+      <DialogContent className="max-w-[90vw] sm:max-w-3xl border-0 shadow-feature-card dark:shadow-feature-card-dark ring-0 text-foreground backdrop-blur-[6px] bg-card/80  overflow-y-auto max-h-[90vh]">
         <DialogHeader>
-          <DialogTitle className="text-white">Tech Stack</DialogTitle>
+          <DialogTitle className="text-foreground">Tech Stack</DialogTitle>
         </DialogHeader>
         <Tabs defaultValue={config.languages[0].type} className="w-full">
-          <TabsList className="w-full flex-wrap justify-start gap-2 bg-BlackRussian text-zinc-400 overflow-x-auto">
+          <TabsList className="w-full flex-wrap justify-start gap-2  text-foreground overflow-x-auto">
             {config.languages.map((category) => (
               <TabsTrigger
                 key={category.type}
                 value={category.type}
-                className="data-[state=active]:bg-Windsor whitespace-nowrap"
+                className="data-[state=active]:bg-secondary data-[state=active]:text-foreground whitespace-nowrap"
               >
                 {category.type}
               </TabsTrigger>
@@ -51,18 +51,18 @@ export const LanguagesDialog: React.FC<LanguagesDialogProps> = ({ trigger }) => 
           </TabsList>
           {config.languages.map((category) => (
             <TabsContent key={category.type} value={category.type}>
-              <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 p-2 sm:p-4">
+              <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 p-2 sm:p-4 ">
                 {category.languages.map((lang) => (
                   <div
                     key={lang.name}
-                    className="flex flex-col items-center justify-center p-2 sm:p-4 rounded-lg bg-BlackRussian hover:bg-Windsor/20 transition-colors"
+                    className="flex flex-col items-center justify-center p-2 sm:p-4 rounded-lg bg-BlackRussian hover:bg-windsor/20 transition-colors"
                   >
                     <img
                       src={lang.img}
                       alt={lang.name}
                       className="w-8 h-8 sm:w-12 sm:h-12 mb-2"
                     />
-                    <span className="text-xs sm:text-sm text-center text-white">{lang.name}</span>
+                    <span className="text-xs sm:text-sm text-center text-foreground">{lang.name}</span>
                   </div>
                 ))}
               </div>
