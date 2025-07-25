2d1959e37863205d7fb6465eed7916ab565f6aa5
BossDaily <gboys2008@gmail.com>
2025-07-25 14:52:29 -0500

    fixed cursor-pointer not  showing

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -18,20 +18,20 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="border-0 ring-0">
-        <Button variant="ghost" className="border-0 shadow-feature-card dark:shadow-feature-card-dark ring-0" size="icon">
+        <Button variant="ghost" className="border-0 shadow-feature-card dark:shadow-feature-card-dark ring-0 cursor-pointer" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="border-0 shadow-feature-card dark:shadow-feature-card-dark ring-0backdrop-blur-[6px] bg-card/80">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
